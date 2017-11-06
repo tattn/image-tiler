3feca3c4ccb4c25c9ee9805313cbe1abaafd93eb
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-11-06 16:18:25 +0900

    Add go files

---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"html/template"
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func main() {
+	fmt.Println("Launching...")
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", upload)
+	mux.HandleFunc("/tile", tile)
+ 
+	server := &http.Server{
+		Addr: "127.0.0.1:8080",
+		Handler: mux,
+	}
+
+	TILES = loadTiles()
+
+	fmt.Println("Started!")
+	server.ListenAndServe()
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("upload.html")
+	t.Execute(w, nil)
+}
+
+func tile(w http.ResponseWriter, r *http.Request) {
+	t0 := time.Now()
+
+	r.ParseMultipartForm(10485760)
+	file, _, _ := r.FormFile("image")
+	defer file.Close()
+
+	tileSize, _ := strconv.Atoi(r.FormValue("tile_size"))
+
+	origImg, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println("failed to decode an uploaded image", err)
+	}
+	bounds := origImg.Bounds()
+	db := cloneTileDB()
+
+	// fan-out
+	c1 := cut(origImg, &db, tileSize, bounds.Min.X, bounds.Min.Y, bounds.Max.X/2, bounds.Max.Y/2)
+	c2 := cut(origImg, &db, tileSize, bounds.Max.X/2, bounds.Min.Y, bounds.Max.X, bounds.Max.Y/2)
+	c3 := cut(origImg, &db, tileSize, bounds.Min.X, bounds.Max.Y/2, bounds.Max.X/2, bounds.Max.Y)
+	c4 := cut(origImg, &db, tileSize, bounds.Max.X/2, bounds.Max.Y/2, bounds.Max.X, bounds.Max.Y)
+
+	// fan-in
+	c := merge(bounds, c1, c2, c3, c4)
+	buf := new(bytes.Buffer)
+	jpeg.Encode(buf, origImg, nil)
+	imgBase64Str := base64.StdEncoding.EncodeToString(buf.Bytes())
+
+	t1 := time.Now()
+	t, _ := template.ParseFiles("results.html")
+	t.Execute(w, map[string]string{
+		"original": imgBase64Str,
+		"tiled": <-c,
+		"duration": fmt.Sprintf("%v ", t1.Sub(t0)),
+	})
+}
+
+func cut(origImg image.Image, db *TileDB, tileSize, x1, y1, x2, y2 int) <- chan image.Image {
+	c := make(chan image.Image)
+	sp := image.Point{0, 0}
+	go func() {
+		newImg := image.NewNRGBA(image.Rect(x1, y1, x2, y2))
+		for y := y1; y < y2; y = y + tileSize {
+			for x := x1; x < x2; x = x + tileSize {
+				r, g, b, _ := origImg.At(x, y).RGBA()
+				color := [3]float64{float64(r), float64(g), float64(b)}
+				nearest := db.nearest(color)
+				file, err := os.Open(nearest)
+				if err == nil {
+					img, _, err := image.Decode(file)
+					if err == nil {
+						t := resize(img, tileSize)
+						tile := t.SubImage(t.Bounds())
+						tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
+						draw.Draw(newImg, tileBounds, tile, sp, draw.Src)
+					} else {
+						fmt.Println("failed to decode a tile file", nearest, err)
+					}
+				} else {
+					fmt.Println("failed to open a tile file", err)
+				}
+				file.Close()
+			}
+		}
+		c <- newImg.SubImage(newImg.Rect)
+	}()
+	return c
+}
+
+func merge(r image.Rectangle, c1, c2, c3, c4 <- chan image.Image) <- chan string {
+	c := make(chan string)
+	go func() {
+		var wg sync.WaitGroup
+		newImg := image.NewNRGBA(r)
+		copy := func(dst draw.Image, r image.Rectangle, src image.Image, sp image.Point) {
+			draw.Draw(dst, r, src, sp, draw.Src)
+			wg.Done()
+		}
+		wg.Add(4)
+		var s1, s2, s3, s4 image.Image
+		var ok1, ok2, ok3, ok4 bool
+		for {
+			select {
+			case s1, ok1 = <-c1:
+				go copy(newImg, s1.Bounds(), s1, image.Point{r.Min.X, r.Min.Y})
+			case s2, ok2 = <-c2:
+				go copy(newImg, s2.Bounds(), s2, image.Point{r.Max.X / 2, r.Min.Y})
+			case s3, ok3 = <-c3:
+				go copy(newImg, s3.Bounds(), s3, image.Point{r.Min.X, r.Max.Y / 2})
+			case s4, ok4 = <-c4:
+				go copy(newImg, s4.Bounds(), s4, image.Point{r.Max.X / 2, r.Max.Y / 2})
+			}
+			if ok1 && ok2 && ok3 && ok4 { break }
+		}
+		wg.Wait()
+		buf := new(bytes.Buffer)
+		jpeg.Encode(buf, newImg, nil)
+		c <- base64.StdEncoding.EncodeToString(buf.Bytes())
+	}()
+	return c
+}
+
---
+++ tile.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	_ "image/jpeg"
+	"io/ioutil"
+	"math"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+type TileDB struct {
+	mutex *sync.Mutex
+	store map[string][3]float64
+}
+
+func (db *TileDB) nearest(target [3]float64) string {
+	var filename string
+	db.mutex.Lock()
+	smallest := 1000000.0
+	for k, v := range db.store {
+		dist := distance(target, v)
+		if dist < smallest {
+			filename, smallest = k, dist
+		}
+	}
+	delete(db.store, filename)
+	db.mutex.Unlock()
+	return filename
+}
+
+var TILES map[string][3]float64
+
+func loadTiles() map[string][3]float64 {
+	fmt.Println("Loading tiles...")
+	tiles := make(map[string][3]float64)
+	files, _ := ioutil.ReadDir("tiles")
+	for _, f := range files {
+		name := filepath.Join("tiles", f.Name())
+		file, err := os.Open(name)
+		if err == nil {
+			img, _, err := image.Decode(file)
+			if err == nil {
+				tiles[name] = averageColor(img)
+			} else {
+				fmt.Println("failed to decode a tile file:", name, err)
+			}
+		} else {
+			fmt.Println("failed to open a tile file:", name, err)
+		}
+		file.Close()
+	}
+	fmt.Println("Loaded tiles!")
+	return tiles
+}
+
+func averageColor(img image.Image) [3]float64 {
+	bounds := img.Bounds()
+	r, g, b := 0.0, 0.0, 0.0
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			ir, ig, ib, _ := img.At(x, y).RGBA()
+			r, g, b = r+float64(ir), g+float64(ig), b+float64(ib)
+		}
+	}
+	pixelCount := float64(bounds.Max.X * bounds.Max.Y)
+	return [3]float64{r/pixelCount, g/pixelCount, b/pixelCount}
+}
+
+func cloneTileDB() TileDB {
+	tiles := make(map[string][3]float64)
+	for k, v := range TILES {
+		tiles[k] = v
+	}
+	return TileDB{
+		mutex: &sync.Mutex{},
+		store: tiles,
+	}
+}
+
+func resize(in image.Image, newWidth int) image.NRGBA {
+	bounds := in.Bounds()
+	width := bounds.Dx()
+	raito := width / newWidth
+	out := image.NewNRGBA(image.Rect(bounds.Min.X/raito, bounds.Min.Y/raito, bounds.Max.X/raito, bounds.Max.Y/raito))
+	for y, j := bounds.Min.Y, bounds.Min.Y; y < bounds.Max.Y; y, j = y+raito, j+1 {
+		for x, i := bounds.Min.X, bounds.Min.X; x < bounds.Max.X; x, i = x+raito, i+1 {
+			r, g, b, a := in.At(x, y).RGBA()
+			out.SetNRGBA(i, j, color.NRGBA{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)})
+		}
+	}
+	return *out
+}
+
+func distance(p1 [3]float64, p2 [3]float64) float64 {
+	return math.Sqrt(sq(p2[0] - p1[0]) + sq(p2[1] - p1[1]) + sq(p2[2] - p1[2]))
+}
+
+func sq(n float64) float64 {
+	return n * n
+}
+
